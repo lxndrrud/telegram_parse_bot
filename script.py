76f0a9e35696f5173319765956ef4eda672d0439
Alexander Rud` <60382252+lxndrrud@users.noreply.github.com>
2021-02-12 22:14:22 +0300

    Update script.py

--- script.py
+++ script.py
@@ -101,10 +101,10 @@ def music_command(message: telebot.types.Message):
     urls, artists, titles = [], [], []
     for i in range(len(funcs)):
         u, a, t = funcs[i](query)
-        urls = urls + u
-        artists = artists + a
-        titles = titles + t
-        #print(urls, artists, titles)
+        urls = urls.extend(u)
+        artists = artists.extend(a)
+        titles = titles.extend(t)
+        print(urls, artists, titles)
     if urls != []:
         markup = telebot.types.InlineKeyboardMarkup(row_width=1)
         with config.connect_to_db() as mydb:
